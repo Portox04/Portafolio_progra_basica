e900e84be4406103caa0d087756108b97dd826a2
Portox04 <96162463+Portox04@users.noreply.github.com>
2024-04-25 22:27:20 -0600

    ejercicio_semana8.py

--- ejercicio_semana8.py
+++ ejercicio_semana8.py
@@ -1,5 +1,36 @@
 #Semana 8
+#Ejercicio 1
+def convertir_binario(numero):
+    resultado_binario = ""
+    while numero > 0:
+        resultado_binario = str(numero % 2) + resultado_binario
+        numero //= 2
+    print("El número en binario es:", resultado_binario)
 
-#Ejercicio #1
+def convertir_octal(numero):
+    resultado_octal = ""
+    while numero > 0:
+        resultado_octal = str(numero % 8) + resultado_octal
+        numero //= 8
+    return resultado_octal
 
+def convertir_hexadecimal(numero):
+    resultado_hexadecimal = ""
+    while numero > 0:
+        residuo = numero % 16
+        if residuo < 10:
+            resultado_hexadecimal = str(residuo) + resultado_hexadecimal
+        else:
+            resultado_hexadecimal = chr(ord('A') + residuo - 10) + resultado_hexadecimal
+        numero //= 16
+    return resultado_hexadecimal
 
+def mostrar_conversiones(numero):
+    print("Número:", numero)
+    convertir_binario(numero)
+    print("Número en octal:", convertir_octal(numero))
+    print("Número en hexadecimal:", convertir_hexadecimal(numero))
+
+# Programa principal
+numero = int(input("Ingrese un número entero: "))
+mostrar_conversiones(numero)
